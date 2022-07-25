f47decb49e9cfbb647d2ad912999867085b6f630
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2022-07-25 17:17:04 +0200

    integrate doctest as unittest framework ("make run_test" to run the tests)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#ifndef DOCTEST_CONFIG_DISABLE
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+#else
+
+#include "doctest/doctest.h"
 #include <iostream>
 
 int main() {
 	std::cout << "Hello World!!" << std::endl;
 	return 0;
+}
+
+#endif
+
+TEST_CASE("sanity check") {
+	REQUIRE(1 < 2);
+	CHECK('a' != 'b');
+	WARN('a'+1 == 'b');
 }
