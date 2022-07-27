6909dbb0e380279a2648332537c32d46be42275e
Mark Orlando Zeller <45102464+MaZe3D@users.noreply.github.com>
2022-07-27 17:22:04 +0200

    added extra newline and removed uneccesary coment

--- src/Game.h
+++ src/Game.h
@@ -8,13 +8,13 @@ public:
     ~Game();
 
     void run();
+
 private:
     SDL_Window* m_window;
     SDL_Renderer* m_renderer;
 
     bool m_isRunning;
 
-    // Methods
     void eventHandler();
     void update();
     void render();
