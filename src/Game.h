9ed344f046c827bff133e5a0aedd5603580060c3
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2022-07-28 17:27:45 +0200

    Revert "11 missing texture loader"

--- src/Game.h
+++ src/Game.h
@@ -1,10 +1,7 @@
 #pragma once
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <string>
-#include <list>
-#include "UIElement.h"
 
 enum class GameState {
 	MENU,
@@ -24,12 +21,6 @@ private:
 	SDL_Renderer* m_renderer;
 
 	GameState m_gameState = GameState::MENU;
-
-	std::list<UIElement*> m_uiElements = std::list<UIElement*>();
-
-	std::list<InteractableUIElement*> m_interactableUIElements = std::list<InteractableUIElement*>();
-
-	void exampleRender();
 
 	void eventHandler();
 	void render();
