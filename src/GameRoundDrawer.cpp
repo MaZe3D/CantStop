a41095c5ace25de72547e5ebf8ca4a616604a2d7
Mark Orlando Zeller <45102464+MaZe3D@users.noreply.github.com>
2022-07-31 16:46:11 +0200

    implemented Bar Drawer

--- src/GameRoundDrawer.cpp
+++ src/GameRoundDrawer.cpp
@@ -11,29 +11,70 @@ GameRoundDrawer::GameRoundDrawer(const std::shared_ptr<Window> window, const std
 	m_background .rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::CENTER);
 	onWindowResized(window->getWidth(), window->getHeight());
 
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < 11; i++) {
 		m_bars.push_back(Bars(TextureDrawable(m_textureBarPlayer1), TextureDrawable(m_textureBarPlayer2), TextureDrawable(m_textureBarTemp)));
 	}
 
 	for (auto& bars : m_bars) {
-		bars.barPlayer1.rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::BOTTOM);
-		bars.barPlayer2.rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::BOTTOM);
-		bars.barTemp   .rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::BOTTOM);
+		bars.barPlayer1.rect.setAnchorModeX(Rect::AnchorMode::LEFT).setAnchorModeY(Rect::AnchorMode::BOTTOM);
+		bars.barPlayer2.rect.setAnchorModeX(Rect::AnchorMode::LEFT).setAnchorModeY(Rect::AnchorMode::BOTTOM);
+		bars.barTemp   .rect.setAnchorModeX(Rect::AnchorMode::LEFT).setAnchorModeY(Rect::AnchorMode::BOTTOM);
 	}
+
+	onWindowResized(window->getWidth(), window->getHeight());
 
 }
 
 void GameRoundDrawer::draw(std::shared_ptr<GameRound>& round) {
 	m_background.draw();
-	
+	setBars(round);
+
+	for(auto &bars : m_bars) {
+		bars.draw();
+	}
 }
 
-void GameRoundDrawer::setBars(GameRound& round) {
-	
+void GameRoundDrawer::setBars(std::shared_ptr<GameRound> round) {
+	for (unsigned int i = 0; i < m_bars.size(); i++) {
+		m_bars[i].barPlayer1.rect.setHeight(round->getBoard().getColumn(i).actor1Marker * m_barIncrement);
+		m_bars[i].barPlayer2.rect.setHeight(round->getBoard().getColumn(i).actor2Marker * m_barIncrement);
+		if (round->getBoard().getColumn(i).runnerOffset > 0) {
+			switch (round->getCurrentActor()) {
+			case ActorEnum::ACTOR1:
+				m_bars[i].barTemp.rect
+					.setPos(m_bars[i].barPlayer1.rect.getPosX(), m_bars[i].barPlayer1.rect.getPosY() - m_bars[i].barPlayer1.rect.getHeight())
+					.setHeight(round->getBoard().getColumn(i).runnerOffset * m_barIncrement);
+				break;
+			case ActorEnum::ACTOR2:
+				m_bars[i].barTemp.rect
+					.setPos(m_bars[i].barPlayer2.rect.getPosX(), m_bars[i].barPlayer2.rect.getPosY() - m_bars[i].barPlayer2.rect.getHeight())
+					.setHeight(round->getBoard().getColumn(i).runnerOffset * m_barIncrement);
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
 
 void GameRoundDrawer::onWindowResized(int width, int height) {
 	m_background.rect.setHeightKeepAspect(height, m_background.getTexture()->getAspect()).setPos(width/2, height/2);
+
+	const int firstBarPosX = (width/2) - (height * (10./27.));
+	const int firstBarPosY = height * (1657./2160.);
+	const int barWidth = height * (1./80.);
+	const int barDistance = height * (152.6/2160.);
+	const int barPairDistance = height * (47./2160.);
+	m_barIncrement = height * (7./135.);
+
+	for (unsigned int i = 0; i < m_bars.size(); i++) {
+		m_bars[i].barPlayer1.rect
+			.setPos(firstBarPosX + (i * barDistance), firstBarPosY)
+			.setWidth(barWidth);
+		m_bars[i].barPlayer2.rect
+			.setPos(firstBarPosX + barPairDistance + (i * barDistance), firstBarPosY)
+			.setWidth(barWidth);
+	}
 }
 
 void GameRoundDrawer::onLeftClick(int32_t x, int32_t y) {
@@ -43,4 +84,10 @@ GameRoundDrawer::Bars::Bars(TextureDrawable barPlayer1, TextureDrawable barPlaye
 	: barPlayer1(barPlayer1)
 	, barPlayer2(barPlayer2)
 	, barTemp(barTemp) {
+}
+
+void GameRoundDrawer::Bars::draw() {
+	barPlayer1.draw();
+	barPlayer2.draw();
+	barTemp.draw();
 }
--- src/GameRoundDrawer.h
+++ src/GameRoundDrawer.h
@@ -12,7 +12,6 @@ public:
 	GameRoundDrawer(const std::shared_ptr<Window> window, const std::shared_ptr<const Font>& font);
 
 	void draw(std::shared_ptr<GameRound>& round);
-	void setBars(GameRound& round);
 
 private:
 
@@ -23,15 +22,21 @@ private:
 		TextureDrawable barTemp;
 
 		Bars(TextureDrawable barPlayer1, TextureDrawable barPlayer2, TextureDrawable barTemp);
+
+		void draw();
 	};
 
 	TextureDrawable m_background;
+
+	int m_barIncrement;
 	
 	const std::shared_ptr<const Texture> m_textureBarPlayer1;
 	const std::shared_ptr<const Texture> m_textureBarPlayer2;
 	const std::shared_ptr<const Texture> m_textureBarTemp;
 
 	std::vector<Bars> m_bars;
+
+	void setBars(std::shared_ptr<GameRound> round);
 
 	void onWindowResized(int width, int height) override;
 	void onLeftClick(int32_t x, int32_t y) override;
