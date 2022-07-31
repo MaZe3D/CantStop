3cb6ea873c2dbd5422eaa767c523cc9fc868c966
Luca Guth <lucaguth@web.de>
2022-07-31 09:41:54 +0200

    logic of GreedyBot

--- src/logic/bots/GreedyBot.cpp
+++ src/logic/bots/GreedyBot.cpp
@@ -1,1 +1,30 @@
 #include "GreedyBot.h"
+
+GreedyBot::GreedyBot() {}
+
+bool GreedyBot::finishedTurn(const Board &board) {
+	for (uint8_t i = 0; i < 11; i++) {
+		if (board.getColumn(i).runnerOffset != 0) {
+			const uint8_t& actorMarker = (this->getActorEnum() == ActorEnum::ACTOR1) ? board.getColumn(i).actor1Marker : board.getColumn(i).actor2Marker;
+			if ((board.getColumn(i).runnerOffset + actorMarker) == board.getColumnHeight(i)) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+uint8_t GreedyBot::choseCombination(const Board &board, const DiceThrow &diceThrow) {
+	uint8_t bestCombination = 0;
+	uint8_t diffrence = 20;
+	for (int i = 0; i < 11; i++) {
+		const uint8_t& actorMarker = (this->getActorEnum() == ActorEnum::ACTOR1) ? board.getColumn(i).actor1Marker : board.getColumn(i).actor2Marker;
+		if (board.getColumnHeight(i) > actorMarker) {
+			if (board.getColumnHeight(i) - actorMarker < diffrence) {
+				bestCombination = i;
+				diffrence = board.getColumnHeight(i) - actorMarker;
+			}
+		}
+	}
+	return bestCombination;
+}
--- src/logic/bots/GreedyBot.h
+++ src/logic/bots/GreedyBot.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "../Actor.h"
 
-class GreedyBot : public Actor{
+class GreedyBot : public Actor {
     GreedyBot();
     uint8_t choseCombination(const Board& board, const DiceThrow& diceThrow) override;
     bool finishedTurn(const Board& board) override;
