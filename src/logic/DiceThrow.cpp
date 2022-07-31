ae6e89027cce8ba94471c936b12c117a05435ebd
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2022-08-01 01:23:06 +0200

    fix another dice combination bug

--- src/logic/DiceThrow.cpp
+++ src/logic/DiceThrow.cpp
@@ -11,37 +11,42 @@ DiceThrow::DiceThrow(const Board& board, const ActorEnum currentActor) {
 			usedRunners[usedRunnerCount++] = i+2;
 	}
 
-	if (usedRunnerCount == 3) return;
-
-
-
 	// throw dice
 	for (uint8_t i = 0; i < 4; i++)
 		m_dice[i] = rand() % 6 + 1;
 
-	uint8_t maxValue = m_dice[0] + m_dice[1] + m_dice[2] + m_dice[3];
+
 
 	// calculate all possible valid combinations for player
+	uint8_t totalDiceSum = m_dice[0] + m_dice[1] + m_dice[2] + m_dice[3];
 	for (int j = 1; j < 4; ++j) {
 		int8_t a = m_dice[0] + m_dice[j];
-		int8_t b = maxValue - a;
+		int8_t b = totalDiceSum - a;
 		const Board::Column& columnA = board.getColumn(a-2);
 		const Board::Column& columnB = board.getColumn(b-2);
 		const uint8_t& actorMarkerA            = (currentActor == ActorEnum::ACTOR1) ? columnA.actor1Marker : columnA.actor2Marker;
 		const uint8_t& actorMarkerAOtherPlayer = (currentActor == ActorEnum::ACTOR1) ? columnA.actor2Marker : columnA.actor1Marker;
 		const uint8_t& actorMarkerB            = (currentActor == ActorEnum::ACTOR1) ? columnB.actor1Marker : columnB.actor2Marker;
 		const uint8_t& actorMarkerBOtherPlayer = (currentActor == ActorEnum::ACTOR1) ? columnB.actor2Marker : columnB.actor1Marker;
-		if (actorMarkerAOtherPlayer == columnA.maxHeight || actorMarkerA + columnA.runnerOffset >= columnA.maxHeight) {
+		if ((actorMarkerAOtherPlayer == columnA.maxHeight) || (actorMarkerA + columnA.runnerOffset >= columnA.maxHeight)) {
 			a = -1;
 		}
-		if (actorMarkerBOtherPlayer == columnB.maxHeight || actorMarkerB + columnB.runnerOffset >= columnB.maxHeight) {
+		if ((actorMarkerBOtherPlayer == columnB.maxHeight) || (actorMarkerB + columnB.runnerOffset >= columnB.maxHeight)) {
 			b = -1;
 		}
-		if (a == b && actorMarkerA + columnA.runnerOffset +1 >= columnA.maxHeight) {
+		if ((a == b) && (actorMarkerA + columnA.runnerOffset +1 >= columnA.maxHeight)) {
 			b = -1;
 		}
 
-		if (usedRunnerCount == 2 && a != usedRunners[0] && a != usedRunners[1] && b != usedRunners[0] && b != usedRunners[1]) {
+		if (usedRunnerCount == 3) {
+			if (a != usedRunners[0] && a != usedRunners[1] && a != usedRunners[2]) {
+				a = -1;
+			}
+			if (b != usedRunners[0] && b != usedRunners[1] && b != usedRunners[2]) {
+				b = -1;
+			}
+		}
+		else if (usedRunnerCount == 2 && a != usedRunners[0] && a != usedRunners[1] && b != usedRunners[0] && b != usedRunners[1]) {
 			if (a > 0) m_combinations[m_combinationCount++] = {a, -1};
 			if (b > 0) m_combinations[m_combinationCount++] = {b, -1};
 			continue;
