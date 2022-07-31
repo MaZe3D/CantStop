7104622c8f8f781a0e08a5bc25bd8124980209a9
Mark Orlando Zeller <45102464+MaZe3D@users.noreply.github.com>
2022-07-31 15:32:20 +0200

    added array

--- src/GameRoundDrawer.cpp
+++ src/GameRoundDrawer.cpp
@@ -3,13 +3,33 @@
 GameRoundDrawer::GameRoundDrawer(const std::shared_ptr<Window> window, const std::shared_ptr<const Font>& font)
 	: Event(window)
 	, m_background("res/sprites/Game_Background.png", window)
+	, m_textureBarPlayer1(window->loadTexture("res/sprites/Game_Player1_Bar.png"))
+	, m_textureBarPlayer2(window->loadTexture("res/sprites/Game_Player2_Bar.png"))
+	, m_textureBarTemp(window->loadTexture("res/sprites/Game_Temp_Bar.png"))
+
 {
 	m_background .rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::CENTER);
 	onWindowResized(window->getWidth(), window->getHeight());
+
+	for (int i = 0; i < 12; i++) {
+		m_bars.push_back(Bars(TextureDrawable(m_textureBarPlayer1), TextureDrawable(m_textureBarPlayer2), TextureDrawable(m_textureBarTemp)));
+	}
+
+	for (auto& bars : m_bars) {
+		bars.barPlayer1.rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::BOTTOM);
+		bars.barPlayer2.rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::BOTTOM);
+		bars.barTemp   .rect.setAnchorModeX(Rect::AnchorMode::CENTER).setAnchorModeY(Rect::AnchorMode::BOTTOM);
+	}
+
 }
 
 void GameRoundDrawer::draw(std::shared_ptr<GameRound>& round) {
 	m_background.draw();
+	
+}
+
+void GameRoundDrawer::setBars(GameRound& round) {
+	
 }
 
 void GameRoundDrawer::onWindowResized(int width, int height) {
@@ -17,4 +37,10 @@ void GameRoundDrawer::onWindowResized(int width, int height) {
 }
 
 void GameRoundDrawer::onLeftClick(int32_t x, int32_t y) {
+}
+
+GameRoundDrawer::Bars::Bars(TextureDrawable barPlayer1, TextureDrawable barPlayer2, TextureDrawable barTemp)
+	: barPlayer1(barPlayer1)
+	, barPlayer2(barPlayer2)
+	, barTemp(barTemp) {
 }
--- src/GameRoundDrawer.h
+++ src/GameRoundDrawer.h
@@ -6,14 +6,32 @@
 #include "drawables/TextDrawable.h"
 #include "logic/GameRound.h"
 
+
 class GameRoundDrawer : WindowResizedEvent, LeftClickEvent {
 public:
 	GameRoundDrawer(const std::shared_ptr<Window> window, const std::shared_ptr<const Font>& font);
 
 	void draw(std::shared_ptr<GameRound>& round);
+	void setBars(GameRound& round);
 
 private:
+
+	struct Bars
+	{
+		TextureDrawable barPlayer1;
+		TextureDrawable barPlayer2;
+		TextureDrawable barTemp;
+
+		Bars(TextureDrawable barPlayer1, TextureDrawable barPlayer2, TextureDrawable barTemp);
+	};
+
 	TextureDrawable m_background;
+	
+	const std::shared_ptr<const Texture> m_textureBarPlayer1;
+	const std::shared_ptr<const Texture> m_textureBarPlayer2;
+	const std::shared_ptr<const Texture> m_textureBarTemp;
+
+	std::vector<Bars> m_bars;
 
 	void onWindowResized(int width, int height) override;
 	void onLeftClick(int32_t x, int32_t y) override;
