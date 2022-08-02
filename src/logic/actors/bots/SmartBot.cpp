95105bb6bef4606fff87501f2d1fdf7bc239a40d
Benedikt Hauser <inf21037@lehre.dhbw-stuttgart.de>
2022-08-02 22:31:32 +0200

    improve smartbot functions

--- src/logic/actors/bots/SmartBot.cpp
+++ src/logic/actors/bots/SmartBot.cpp
@@ -1,6 +1,36 @@
 #include "SmartBot.h"
 
 uint8_t SmartBot::choseCombination(const Board& board, const DiceThrow& diceThrow, MersenneTwister& rand) {
+	uint8_t bestCombination = 0;
+	float rating = 0;
+	uint8_t i = 0;
+	uint8_t distanceA = 0;
+	uint8_t distanceB = 0;
+	for (uint8_t combinationID = 0; combinationID < diceThrow.getCombinationCount(); combinationID++) {
+		float tempRating = 0;
+		const DiceThrow::Combination& combination = diceThrow.getCombination(combinationID);
+		if(this->getActorEnum() == ActorEnum::ACTOR1){
+			distanceA = board.getColumn(combination.a - 2).maxHeight - board.getColumn(combination.a - 2).actor1Marker - board.getColumn(combination.a - 2).runnerOffset;
+			if(combination.b > 0) distanceB = board.getColumn(combination.b - 2).maxHeight - board.getColumn(combination.b - 2).actor1Marker - board.getColumn(combination.b - 2).runnerOffset;
+		} else {
+			distanceA = board.getColumn(combination.a - 2).maxHeight - board.getColumn(combination.a - 2).actor2Marker - board.getColumn(combination.a - 2).runnerOffset;
+			if(combination.b > 0)distanceB = board.getColumn(combination.b - 2).maxHeight - board.getColumn(combination.b - 2).actor2Marker - board.getColumn(combination.b - 2).runnerOffset;
+		}
+		if(combination.a != -1 && combination.b != -1) {
+				tempRating = ((2*board.getColumn(combination.a - 2).maxHeight-distanceA) + 
+				(2*board.getColumn(combination.b - 2).maxHeight-distanceB)/2);
+		}
+		if(combination.b == -1) {
+			tempRating = (2 * board.getColumn(combination.a - 2).maxHeight - distanceA);
+		}
+		if(tempRating > rating) {
+			bestCombination = i;
+			rating = tempRating;
+		}
+		i++;
+	}
+	return bestCombination;
+	/*
 	uint8_t choice = 0;
 	uint8_t distance = -1;
 	for (int i = 0; i < diceThrow.getCombinationCount(); i++) {
@@ -22,15 +52,36 @@ uint8_t SmartBot::choseCombination(const Board& board, const DiceThrow& diceThro
 			}
 		}
 	}
-	return choice;
+	return choice;*/
 }
 
 bool SmartBot::finishedTurn(const Board& board, MersenneTwister& rand) {
 	uint8_t usedMarkers = 0;
-	for (int i = 0; i < 11; ++i) {
+	uint8_t riskfactor = 0;
+	uint8_t weight = 0;
+	for (int i = 0; i < 11; i++) {
 		if (board.getColumn(i).runnerOffset > 0) {
 			usedMarkers++;
+			weight += board.getColumn(i).maxHeight;
+			riskfactor += board.getColumn(i).runnerOffset;
 		}
 	}
-	return (usedMarkers == 3);
+	if(usedMarkers != 3) {
+		return false;
+	}
+	else if (weight < 15) {
+		return true;
+	}
+	else if(riskfactor > 9) {
+		return true;
+	}
+	else if(weight < 24 && riskfactor > 6) {
+		return true;
+	}
+	else if (weight > 30) {
+		return false;
+	}
+	else {
+		return true;
+	}
 }
