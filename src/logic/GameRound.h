69ebf5942bffbe7cd208abe6dad45656f9dcbfbc
Mark Orlando Zeller <45102464+MaZe3D@users.noreply.github.com>
2022-08-02 21:14:22 +0200

    tested GameRound

--- src/logic/GameRound.h
+++ src/logic/GameRound.h
@@ -4,6 +4,8 @@
 #include "Board.h"
 #include "ActorEnum.h"
 #include "util/MersenneTwister.h"
+#include <doctest.h>
+#include "actors/bots/GreedyBot.h"
 
 class GameRound {
 public:
@@ -38,3 +40,52 @@ private:
 	std::shared_ptr<Actor> m_actor2;
 	DiceThrow m_diceThrow;
 };
+
+TEST_CASE("GameRound") {
+	MersenneTwister rand = MersenneTwister(0);
+	auto actor1 = std::make_shared<GreedyBot>();
+	auto actor2 = std::make_shared<GreedyBot>();
+	GameRound gameRound(actor1, actor2, rand);
+	SUBCASE("First Actor is ACTOR1")
+	{
+		CHECK(gameRound.getCurrentActorEnum() == ActorEnum::ACTOR1);
+	}
+
+	SUBCASE("Next Step is CHOOSE_DICE_COMBINATION")
+	{
+		CHECK(gameRound.getNextStep() == GameRound::NextStep::CHOOSE_DICE_COMBINATION);
+	}
+
+	SUBCASE("Next Step is CHOOSE_TO_CONTINUE_OR_STOP")
+	{
+		gameRound.nextStep();
+		CHECK(gameRound.getNextStep() == GameRound::NextStep::CHOOSE_TO_CONTINUE_OR_STOP);
+	}
+
+	SUBCASE("Next Step is CHOOSE_DICE_COMBINATION")
+	{
+		gameRound.nextStep();
+		gameRound.nextStep();
+		CHECK(gameRound.getNextStep() == GameRound::NextStep::CHOOSE_DICE_COMBINATION);
+	}
+
+	SUBCASE("ACTOR2 is current actor")
+	{
+		gameRound.nextStep();
+		gameRound.nextStep();
+		gameRound.nextStep();
+		gameRound.nextStep();
+		gameRound.nextStep();
+		CHECK(gameRound.getCurrentActorEnum() == ActorEnum::ACTOR2);
+	}
+
+	SUBCASE("Game is over")
+	{
+		int i = 0;
+		for (i = 0; i < 126; i++) {
+			CHECK(gameRound.isOver() == false);
+			gameRound.nextStep();
+		}
+		CHECK(gameRound.isOver() == true);
+	}
+}
