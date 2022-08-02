7c8cc616ce790fbf007f16835e8f08211848da5f
Mark Orlando Zeller <45102464+MaZe3D@users.noreply.github.com>
2022-08-02 11:20:18 +0200

    integrated Smartbot

--- src/logic/actors/bots/SmartBot.cpp
+++ src/logic/actors/bots/SmartBot.cpp
@@ -1,9 +1,36 @@
 #include "SmartBot.h"
 
 uint8_t SmartBot::choseCombination(const Board& board, const DiceThrow& diceThrow, MersenneTwister& rand) {
-	return 0;
+	uint8_t choice = 0;
+	uint8_t distance = -1;
+	for (int i = 0; i < diceThrow.getCombinationCount(); i++) {
+		auto combination = diceThrow.getCombination(i);
+		if (combination.a != -1 && combination.b != -1) {
+			if ((7 - combination.a) + (7 - combination.b) < distance) {
+				choice = i;
+				distance = (7 - combination.a) + (7 - combination.b);
+			}
+		} else if (combination.a != -1) {
+			if ((7 - combination.a) * 2 < distance) {
+				choice = i;
+				distance = (7 - combination.a) * 2;
+			}
+		} else if (combination.b != -1) {
+			if ((7 - combination.b) * 2 < distance) {
+				choice = i;
+				distance = (7 - combination.b) * 2;
+			}
+		}
+	}
+	return choice;
 }
 
 bool SmartBot::finishedTurn(const Board& board, MersenneTwister& rand) {
-	return true;
+	uint8_t usedMarkers = 0;
+	for (int i = 0; i < 11; ++i) {
+		if (board.getColumn(i).runnerOffset > 0) {
+			usedMarkers++;
+		}
+	}
+	return (usedMarkers == 3);
 }
